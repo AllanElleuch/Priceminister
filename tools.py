854a5c0a4bfdce25b281fbbfb70e42a9fb6fdf34
Allan Elleuch <allan.elleuch@gmail.com>
2017-09-26 21:51:53 -0400

    Ajout a la classe toolKit la possibilité d'afficher des graphe et charger des csv

--- tools.py
+++ tools.py
@@ -35,40 +35,66 @@ class toolKit:
 
     def drawGraphReviewlength(self):
         listInputReviewContent = self.dataFrame[['review_content','Target']]
-        TabReviewLength =[]
 
-        listInputReviewContent.groupby('Target').plot.hist()
+        self.dataFrame['len'] = [len(x) for x in self.dataFrame['review_content']]
 
-        for review in listInputReviewContent['review_content'].values.tolist():
-            TabReviewLength.append(len(review))
-        # print(TabReviewLength)
+        # print(self.dataFrame)
+        TabReviewLength =self.dataFrame['len'].values.tolist()
 
+        df=self.dataFrame
+        test = np.where(df['Target']==1,df['len'],None)
+        # print(df.loc[df['Target'] == 1]['Target'])
+
+        print(self.dataFrame.groupby(['Target']).len.mean() )
+
+        # colors = {'D':'red', 'E':'blue', 'F':'green', 'G':'black'}
+
+        # drinks.groupby('continent').mean().plot(kind='bar')
+        # grouped = df.groupby('color')
+        # for key, group in grouped:
+        #     group.plot(ax=ax, kind='scatter', x='carat', y='price', label=key, color=colors[key])
+        #
+        # plt.show()
+
+        # dicoReviewLength = {i : 1 if 1==1 else i:1  for i in TabReviewLength  }
+
+        # transforme un tableau en un tuple valeur : nombre d'occurence
         dicoReviewLength={}
         for i in TabReviewLength:
-            if not i in dicoReviewLength:
-                dicoReviewLength[i]=1
-            else:
-                dicoReviewLength[i]+=1
-        x,y=[],[]
+            dicoReviewLength[i] =   dicoReviewLength[i]+1 if i in dicoReviewLength else 1
+        x,y = list(dicoReviewLength.keys()),list(dicoReviewLength.values())
 
-        for key,value in dicoReviewLength.items():
-            x.append(key)
-            y.append(value)
+        # x,y=[],[]
+        # print(TabReviewLength)
+        # for key,value in dicoReviewLength.items():
+        #     x.append(key)
+        #     y.append(value)
 
-        print(dicoReviewLength)
+        # print(x[0:10])
+        # dicoReviewLength={}
+        # for i in TabReviewLength:
+        #     if not i in dicoReviewLength:
+        #         dicoReviewLength[i]=1
+        #     else:
+        #         dicoReviewLength[i]+=1
+
+
+
+
+        # print(len(x))
 
 
         valx=x
         values = y
         slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x,y)
-        print(r_value)
+        # print(r_value)
         fig, ax = plt.subplots()
         plt.bar(valx, values)
         plt.suptitle('product review length')
         plt.ylabel('Number of reviews')
         plt.xlabel('review length')
 
-        plt.show()
+        # plt.show()
 
 csv_delimiter = ';'
 
